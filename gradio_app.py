7236de9097b32e2b171290590d9a4a3dda1a8783
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-09-25 00:42:08 +0530

    feat: created character chatbot

--- gradio_app.py
+++ gradio_app.py
@@ -2,9 +2,10 @@ import gradio as gr
 from theme_classifier import ThemeClassifier
 from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 from text_classification import JutsuClassifier
+from character_chatbot import CharacterChatBot
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
 
 def get_themes(theme_list_str,subtitles_path,save_path):
     theme_list = theme_list_str.split(',')
@@ -51,6 +52,15 @@ def classify_text(text_classifcation_model,text_classifcation_data_path,text_to_
     
     return output
 
+def chat_with_character_chatbot(message, history):
+    character_chatbot = CharacterChatBot("AbdullahTarek/Naruto_Llama-3-8B",
+                                         huggingface_token = os.getenv('huggingface_token')
+                                         )
+
+    output = character_chatbot.chat(message, history)
+    output = output['content'].strip()
+    return output
+
 
 def main():
     with gr.Blocks() as iface:
@@ -95,6 +105,11 @@ def main():
                         classify_text_button = gr.Button("Clasify Text (Jutsu)")
                         classify_text_button.click(classify_text, inputs=[text_classifcation_model,text_classifcation_data_path,text_to_classify], outputs=[text_classification_output])
 
+        # Character Chatbot Section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Character Chatbot</h1>")
+                gr.ChatInterface(chat_with_character_chatbot)
 
     iface.launch(share=True)
             
